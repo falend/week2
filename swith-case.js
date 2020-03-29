392768b9f2a068d0c35b4e6c74b9c949f64963f4
falend <55618978+falend@users.noreply.github.com>
2020-03-30 02:07:09 +0800

    swith-case

---
+++ swith-case.js
@@ -0,0 +1,97 @@
+var tanggal = 14; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
+var bulan = 4; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
+var tahun = 2000; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
+
+if(tanggal = "14"){
+  console.log(tanggal);
+}
+switch(bulan){
+  case 1:
+  alert("Januari");
+  break;
+  case 2:
+  alert("Februari");
+  break;
+  case 3:
+  alert("Maret");
+  break;
+  case 4:
+  alert("April");
+  break;
+  case 5:
+  alert("Mei");
+  break;
+  case 6:
+  alert("Juni");
+  break;
+  case 7:
+  alert("Juli");
+  break;
+  case 8:
+  alert("Agustus");
+  break;
+  case 9:
+  alert("September");
+  break;
+  case 10:
+  alert("Oktober");
+  break;
+  case 11:
+  alert("November");
+  break;
+  case 12:
+  alert("Desember");
+  break;
+console.log(bulan)
+}
+if(tahun = "2000"){
+  console.log(tahun);
+var tanggal = 14; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
+var bulan = 4; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
+var tahun = 2000; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
+
+if(tanggal = "14"){
+  console.log(tanggal);
+}
+switch(bulan){
+  case 1:
+  alert("Januari");
+  break;
+  case 2:
+  alert("Februari");
+  break;
+  case 3:
+  alert("Maret");
+  break;
+  case 4:
+  alert("April");
+  break;
+  case 5:
+  alert("Mei");
+  break;
+  case 6:
+  alert("Juni");
+  break;
+  case 7:
+  alert("Juli");
+  break;
+  case 8:
+  alert("Agustus");
+  break;
+  case 9:
+  alert("September");
+  break;
+  case 10:
+  alert("Oktober");
+  break;
+  case 11:
+  alert("November");
+  break;
+  case 12:
+  alert("Desember");
+  break;
+console.log(bulan)
+}
+if(tahun = "2000"){
+  console.log(tahun);
+}
