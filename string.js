aed676584a2c6af1482916fdd0757984d1e17973
falend <55618978+falend@users.noreply.github.com>
2020-03-30 09:42:51 +0800

    1-4

---
+++ string.js
@@ -0,0 +1,54 @@
+//skeleton code1
+/*var word = 'JavaScript';
+var second = 'is';
+var third = 'awesome';
+var fourth = 'and';
+var fifth = 'I';
+var sixth = 'love';
+var seventh = 'it!';
+var all = word + ' ' + second + ' ' + third + ' ' + fourth + ' ' + fifth + ' ' + sixth + ' ' + seventh;
+console.log(all);
+
+//skeleton code2
+var word = 'wow JavaScript is so cool';
+var exampleFirstWord = word[0] + word[1] + word[2];
+var secondWord = word[4] + word[5] + word[6] + word[7] + word[8] + word[9] + word[10] + word[11] + word[12] + word[13] + word[14] ; 
+var thirdWord = word[15] + word[16]; 
+var fourthWord = word[17] + word[18]; 
+var fifthWord = word[19] + word[20] + word[21] + word[22]; 
+
+console.log('First Word: ' + exampleFirstWord);
+console.log('Second Word: ' + secondWord);
+console.log('Third Word: ' + thirdWord);
+console.log('Fourth Word: ' + fourthWord);
+console.log('Fifth Word: ' + fifthWord);
+
+//skeleton code3
+var word3 = 'wow JavaScript is so cool';
+var exampleFirstWord3 = word.substring(0, 3);
+var secondWord3 = word.substring(4, 14); // do your own!
+var thirdWord3 = word.substring(15, 17); // do your own!
+var fourthWord3 = word.substring(18, 20); // do your own!
+var fifthWord3 = word.substring(21, 24); // do your own!
+
+console.log('First Word: ' + exampleFirstWord);
+console.log('Second Word: ' + secondWord);
+console.log('Third Word: ' + thirdWord);
+console.log('Fourth Word: ' + fourthWord);
+console.log('Fifth Word: ' + fifthWord);*/
+
+/*
+var word4 = 'wow JavaScript is so cool';
+var exampleFirstWord4 = word.substring(0, 3);
+var secondWord3 = word.substring(4, 14); 
+var thirdWord3 = word.substring(15, 17); 
+var fourthWord3 = word.substring(18, 20); 
+var fifthWord3 = word.substring(21, 25);
+
+var firstWordLength = exampleFirstWord4.length;
+
+console.log('First Word: ' + exampleFirstWord + ', with length: ' + exampleFirstWord.length);
+console.log('Second Word: ' + secondWord + ', with length: ' + secondWord.length);
+console.log('Third Word: ' + thirdWord + ', with length: ' + thirdWord.length);
+console.log('Fourth Word: ' + fourthWord + ', with length: ' + fourthWord.length);
+console.log('Fifth Word: ' + fifthWord + ', with length: ' + fifthWord3.length);*/
